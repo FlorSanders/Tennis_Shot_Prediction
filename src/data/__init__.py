487d33c63ee157b8390b38f2c6890bfa8b4a9582
Flor Sanders <me@florsanders.be>
2024-03-23 11:00:24 -0400

    Change order of comment, add data path constant

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,6 +1,9 @@
-# Enable importing from src directory
 import os
 import sys
 
+# Enable importing from src directory
 base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(base_path)
+
+# Constants
+data_path = os.path.abspath(os.path.join(base_path, "..", "data"))
