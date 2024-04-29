e12e09d8b3f9c65cb443f2650b2ebf39787fe09d
Tawab Safi <asaficontact@gmail.com>
2024-04-29 17:53:54 -0400

    Updated the Training Code to include the 3 functionalities mentioned in the description below (#7)
    * Working to put get the model training, the train and val loader are setup, need to modify the trainer and the model to work properly with the data format
    
    * graphs can be complicated - still needs some work before we can start pretraining  - working on figuring out how to batching for graphs by frame and then by num batches
    
    * Its Trainingg!! Please find all the code you need to train in PreTrain.ipynb
    
    * Okay, Now the Training Loop Looks way more organized!
    
    * Applied Masking for Padded items in the sequence during training
    
    * Added functionality to the PreTrainer class for saving the model based on the lowest validation loss seen during training.
    
    * Added function to check for the validity of train and val dataset. Annotations['is_valid'] for items that have an invalid format are set to False so that they are not loaded.

--- src/train/PreTrainer.py
+++ src/train/PreTrainer.py
@@ -19,12 +19,15 @@ from torch.nn.utils.rnn import pad_sequence
 from torch_geometric.data import Batch
 from torch_geometric.data import Data
 from tqdm import tqdm
+import glob
+import json 
+import numpy as np 
 from data import TennisDataset
 from model_builder import build_tennis_embedder
 
 
 class PreTrainer:
-    def __init__(self, model_config_path, train_path, val_path, batch_size, lr, epochs):
+    def __init__(self, model_config_path, train_path, val_path, batch_size, lr, epochs, model_save_path = 'trained_models'):
         self.model = build_tennis_embedder(model_config_path)
         self.train_loader = self.get_data_loader(train_path, batch_size, train = True)
         self.val_loader = self.get_data_loader(val_path, batch_size, train = False)
@@ -35,7 +38,10 @@ class PreTrainer:
         print('Using device:', self.device)
         self.model.to(self.device)
         self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
-        self.criterion = nn.MSELoss()  # Mean Squared Error Loss
+        self.criterion = nn.MSELoss(reduction='none')  # Mean Squared Error Loss
+        self.model_save_path = model_save_path
+        self.best_val_loss = float('inf')
+        print('Using device:', self.device)
     def train(self):
         
         for epoch in range(self.epochs):
@@ -44,48 +50,70 @@ class PreTrainer:
             val_loader = self.val_loader
             self.model.train()
             running_loss = 0.0
-
-            for pose3d, position2d, pose_graph, targets in train_loader: 
-
+            for pose3d, position2d, pose_graph, targets, mask in tqdm(train_loader): 
                 global_positions = position2d
                 graphs = pose_graph
+                mask = mask.unsqueeze(-1).unsqueeze(-1).expand(-1, -1, 17, 3)
 
                 #Move the data to the device
                 graphs = [graph.to(self.device) for graph in graphs]
                 global_positions = global_positions.to(self.device)
                 targets = targets.to(self.device)
+                mask = mask.to(self.device)
 
                 #Optimize the model
+
+                # Forward Pass
                 self.optimizer.zero_grad()
                 outputs = self.model(graphs, global_positions)
+
+                # Compute Loss and Mask it
                 loss = self.criterion(outputs, targets)
-                loss.backward()
+                masked_loss = loss * mask
+                final_loss = masked_loss.sum() / mask.sum() # Reduce the loss; sum and then divide by the number of unmasked elements
+
+                # Backward Pass
+                final_loss.backward()
                 self.optimizer.step()
 
-                running_loss += loss.item()
+                running_loss += final_loss.item()
             
             print(f'Epoch {epoch+1}, Loss: {running_loss/len(train_loader)}')
             
             self.evaluate(val_loader)
+
             
     def evaluate(self, data_loader):
         self.model.eval()
         total_loss = 0.0
         with torch.no_grad():
-            for pose3d, position2d, pose_graph, targets in data_loader:
+            for pose3d, position2d, pose_graph, targets, mask in data_loader:
                 global_positions = position2d
                 graphs = pose_graph
+                mask = mask.unsqueeze(-1).unsqueeze(-1).expand(-1, -1, 17, 3)
 
                 #Move the data to the device
                 graphs = [graph.to(self.device) for graph in graphs]
                 global_positions = global_positions.to(self.device)
                 targets = targets.to(self.device)
+                mask = mask.to(self.device)
                 
                 outputs = self.model(graphs, global_positions)
                 loss = self.criterion(outputs, targets)
-                total_loss += loss.item()
+                masked_loss = loss * mask
+                final_loss = masked_loss.sum() / mask.sum() # Reduce the loss; sum and then divide by the number of unmasked elements
+                total_loss += final_loss.item()
         
-        print(f'Test Loss: {total_loss/len(data_loader)}')
+        avg_val_loss = total_loss / len(data_loader)
+        print(f'Validation Loss: {avg_val_loss}')
+
+        # Check directory and save model if this is the best validation loss so far
+        if avg_val_loss < self.best_val_loss:
+            self.best_val_loss = avg_val_loss
+            if not os.path.exists(self.model_save_path):
+                os.makedirs(self.model_save_path)
+            torch.save(self.model.state_dict(), os.path.join(self.model_save_path, 'best_model.pth'))
+            print(f'Saving model at {self.model_save_path} with validation loss of {avg_val_loss}')
     
     def get_data_loader(self, data_path, batch_size, train = True):
         dataset = TennisDataset(labels_path = data_path)
@@ -121,11 +149,12 @@ def my_collate_fn(batch):
     targets = []
     all_graphs = []
     graph_counts = []  # To count graphs per item in the batch
+    masks = []
 
 
     for item in batch:
         pose3d_item, position2d_item, pose_graph_items, target_item = item
-        
+      
         # Check for None values and correct shapes
         if pose3d_item is not None and position2d_item is not None and pose_graph_items is not None: 
             if pose3d_item.dtype != np.object_ and position2d_item.dtype != np.object_ and len(pose_graph_items) > 0:
@@ -156,6 +185,8 @@ def my_collate_fn(batch):
                     pose3d.append(torch.tensor(pose3d_item, dtype=torch.float32))
                     position2d.append(torch.tensor(position2d_item, dtype=torch.float32))
                     targets.append(torch.tensor(target_item, dtype=torch.float32))
+                    masks.append(torch.ones(len(pose_graph_items), dtype=torch.bool))  # Mask of ones where data is valid
+
                 # else:
                 #     print(f"Skipping due to incorrect dimensions - Pose3D: {pose3d_item.shape}, Position2D: {position2d_item.shape}")
             # else: 
@@ -166,9 +197,10 @@ def my_collate_fn(batch):
     #print("Graphs per item in batch:", graph_counts)  # For Debugging
 
     # Pad pose3d and position2d sequences if not empty
-    pose3d_padded = pad_sequence(pose3d, batch_first=True) if pose3d else torch.Tensor()
-    position2d_padded = pad_sequence(position2d, batch_first=True) if position2d else torch.Tensor()
-    targets_padded = pad_sequence(targets, batch_first=True) if targets else torch.Tensor()
+    pose3d_padded = pad_sequence(pose3d, batch_first=True, padding_value=0.0) if pose3d else torch.Tensor()
+    position2d_padded = pad_sequence(position2d, batch_first=True, padding_value=0.0) if position2d else torch.Tensor()
+    targets_padded = pad_sequence(targets, batch_first=True, padding_value=0.0) if targets else torch.Tensor()
+    mask_padded = pad_sequence(masks, batch_first=True, padding_value=0)  # Pad mask with zeros
 
     #print("Number of Graphs:", len(all_graphs))
     
@@ -181,4 +213,66 @@ def my_collate_fn(batch):
     else:
         batched_graphs = []
 
-    return pose3d_padded, position2d_padded, batched_graphs, targets_padded
+    return pose3d_padded, position2d_padded, batched_graphs, targets_padded, mask_padded
+
+
+
+
+
+def validate_data_format(labels_path):
+    annotation_files = glob.glob(os.path.join(labels_path, "*_info.json"))
+    modified_files = []
+
+    for annotation_file in annotation_files:
+        with open(annotation_file, 'r') as file:
+            annotation = json.load(file)
+        
+        # Run checks for annotations that are true
+        if annotation['is_valid'] == True:
+            is_valid = True
+
+            # Data file paths
+            item_name = os.path.basename(annotation_file).replace("_info.json", "")
+            positions_2d_path = os.path.join(labels_path, f"{item_name}_player_btm_position.npy")
+            poses_3d_path = os.path.join(labels_path, f"{item_name}_player_btm_pose_3d.npy")
+            
+            # Check if data files exist
+            if not os.path.exists(positions_2d_path) or not os.path.exists(poses_3d_path):
+                is_valid = False
+            else:
+                # Load data files
+                positions_2d = np.load(positions_2d_path, allow_pickle=True)
+                poses_3d = np.load(poses_3d_path, allow_pickle=True)
+
+                # if item_name in ["V006_0066", "V006_0178", "V009_1046", "V009_1639"]:
+                #     print("Position_2d_shape: ", positions_2d.shape)
+                #     print("Poses_3d_shape: ", poses_3d.shape)
+                
+                # Check data dimensions
+                if positions_2d.ndim != 2 or positions_2d.shape[1] != 2:
+                    is_valid = False
+                if poses_3d.ndim != 3 or poses_3d.shape[1:] != (17, 3):
+                    is_valid = False
+                
+                # Check for None values and data types
+                if positions_2d.dtype == np.object_ or poses_3d.dtype == np.object_:
+                    is_valid = False
+                if positions_2d is None or poses_3d is None:
+                    is_valid = False
+
+                # Additional checks like empty pose graphs can be added here if they are saved and loaded similarly
+                
+            # Update annotation if invalid
+            if not is_valid:
+                annotation['is_valid'] = False
+                modified_files.append(annotation_file)
+                with open(annotation_file, 'w') as file:
+                    json.dump(annotation, file)
+            
+    print(f"Total files: {len(annotation_files)}")
+    print(f"Invalid Files: {len(modified_files)}")
+    print(f"Valid Files: {len(annotation_files) - len(modified_files)}")
+    print(f"Percentage of valid files: {(len(annotation_files) - len(modified_files)) / len(annotation_files) * 100}%")
+
+    return modified_files
+
